ab179e014ab8fd0f869f9a36a66ca65b155ae8b2
dv <dvaug86@gmail.com>
2023-02-09 15:39:03 -0500

    tweak didn't work

--- pages/components/HomePage.jsx
+++ pages/components/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
-import TitlePic from '../../public/assets/TitlePic.jpg'
+// import TitlePic from '../../public/assets/TitlePic.jpg'
 
 const HomePage = () => {
   return (
     <div className='flex  justify-center  w-full bg-fixed bg-center bg-cover  '>
         {/* overlay */}
         <div className='absolute top-0 left-0 right-0 bottom-0' />
-        <Image src={TitlePic} height="auto" width="100%" alt='/' />
+        {/* <Image src={TitlePic} height="auto" width="100%" alt='/' /> */}
     </div>
   )
 
