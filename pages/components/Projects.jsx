afc216a3cfa9d70b6756b92430212ded4221f08b
dv <dvaug86@gmail.com>
2023-02-10 08:54:13 -0500

    trying2

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -1,39 +1,39 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import DrewsPlant from "../../public/assets/projectImgs/drewsPlant.png";
-import GymApp from "../../public/assets/projectImgs/gymApp.png";
-import ReactTetris from "../../public/assets/projectImgs/reactTetris.png";
-import ProjectItem from "./ProjectItem";
-const Projects = () => {
-  return (
-    <div id="projects" className="w-full">
-      <div className="max-w-[1240px] mx-auto px-2 py-16">
-        <p className="text-xl tracking-widest uppercase text-black ">
-          Projects
-        </p>
-        <h2 className="py-4">What I've Built</h2>
-        <div className="grid md:grid-cols-2 gap-8">
-          <ProjectItem
-            title="Drew's Plant Nursery"
-            backgroundImg={DrewsPlant}
-            projectUrl="/drews"
-          />
-          <ProjectItem
-            title="Gym App"
-            backgroundImg={GymApp}
-            projectUrl="/gym"
-          />
-          <ProjectItem
-            title="Tetris with React"
-            backgroundImg={ReactTetris}
-            projectUrl="/tetris"
-          />
+// import Image from "next/image";
+// import Link from "next/link";
+// import React from "react";
+// import DrewsPlant from "../../public/assets/projectImgs/drewsPlant.png";
+// import GymApp from "../../public/assets/projectImgs/gymApp.png";
+// import ReactTetris from "../../public/assets/projectImgs/reactTetris.png";
+// import ProjectItem from "./ProjectItem";
+// const Projects = () => {
+//   return (
+//     <div id="projects" className="w-full">
+//       <div className="max-w-[1240px] mx-auto px-2 py-16">
+//         <p className="text-xl tracking-widest uppercase text-black ">
+//           Projects
+//         </p>
+//         <h2 className="py-4">What I've Built</h2>
+//         <div className="grid md:grid-cols-2 gap-8">
+//           <ProjectItem
+//             title="Drew's Plant Nursery"
+//             backgroundImg={DrewsPlant}
+//             projectUrl="/drews"
+//           />
+//           <ProjectItem
+//             title="Gym App"
+//             backgroundImg={GymApp}
+//             projectUrl="/gym"
+//           />
+//           <ProjectItem
+//             title="Tetris with React"
+//             backgroundImg={ReactTetris}
+//             projectUrl="/tetris"
+//           />
           
-        </div>
-      </div>
-    </div>
-  );
-};
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Projects;
+// export default Projects;
