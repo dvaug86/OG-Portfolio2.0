b56981dbdcbee1e188cfc679126f36b786a473ce
dv <dvaug86@gmail.com>
2023-02-10 08:49:00 -0500

    gym fix

--- pages/gym.jsx
+++ pages/gym.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import gymApp from '../public/assets/projectImgs/gymApp.png'
+ import gymApp from '../public/assets/projectImgs/gymApp.png'
 import {RiRadioButtonFill} from 'react-icons/ri'
 import Link from "next/link";
 
