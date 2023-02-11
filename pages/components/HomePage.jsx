6b9143b730e1655c2ddce0510d5d5f29b4e80967
dv <dvaug86@gmail.com>
2023-02-11 15:52:38 -0500

    changed hero page

--- pages/components/HomePage.jsx
+++ pages/components/HomePage.jsx
@@ -4,11 +4,15 @@ import TitlePic from '../../public/assets/TitlePic.JPG'
 
 const HomePage = () => {
   return (
-    <div className='flex  justify-center  w-full bg-fixed bg-center bg-cover  '>
-        {/* overlay */}
-        <div className='absolute top-0 left-0 right-0 bottom-0' />
-        <Image src={TitlePic} height="auto" width="100%" alt='/' />
-    </div>
+    // <div className='flex  justify-center  w-full bg-fixed bg-center bg-cover  '>
+    //     {/* overlay */}
+    //     <div className='absolute top-0 left-0 right-0 bottom-0' />
+    //     <Image src={TitlePic} height="auto" width="100%" alt='/' />
+    // </div>
+    <div className='flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img'>
+    {/* overlay */}
+    <div className='absolute top-0 left-0 right-0 bottom-0 z-[2]' />
+</div>
   )
 
 
