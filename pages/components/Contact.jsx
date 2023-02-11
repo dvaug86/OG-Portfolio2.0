97a1d032dafbfa023ad17f315b50c09eb698af62
dv <dvaug86@gmail.com>
2023-02-11 15:21:42 -0500

    udated form and hero

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -4,27 +4,40 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
+import { useForm } from "react-hook-form";
+import finalImg from '../../public/assets/finalImg.JPG';
+
 import Link from "next/link";
 // import finalImg from '../../public/assets/finalImg.jpg'
 
 const Contact = () => {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
   return (
     <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
-        <p className="text-xl tracking-widest upercase text-black">
-          contact
-        </p>
+        <p className="text-xl tracking-widest upercase text-black">contact</p>
         <h2 className="py-4">Get In Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
           {/* left */}
           <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-black rounded-xl p-4 ">
             <div className="lg:p-4 h-full ">
               <div>
-                {/* <Image
+                <Image
                   className="rounded-xl hover:scale-105 ease-in duration-300"
                   src={finalImg}
                   alt="/"
-                /> */}
+                />
               </div>
               <div>
                 <h2 className="py-2">David</h2>
@@ -58,31 +71,53 @@ const Contact = () => {
 
           <div className="col-span-3 w-full h-auto shadow-xl shadow-black rounded-xl lg:p-4">
             <div className="p-4">
-              <form>
+              <form
+                target="_blank"
+                onSubmit={onSubmit}
+                action="https://formsubmit.co/c84de36b3c46270817e653961d0b55ac" 
+                method="POST"
+              >
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Name</label>
                     <input
                       className="border-2 rounded-lg p-3 border-gray-300"
                       type="text"
+                      placeholder="NAME"
+                      {...register("name", {
+                        required: true,
+                        maxLength: 100,
+                      })}
                     />
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">
-                      phone Number
-                    </label>
-                    <input
-                      className="border-2 rounded-lg p-3 border-gray-300"
-                      type="text"
-                    />
+                    {errors.name && (
+                      <p className="text-red mt-1">
+                        {errors.name.type === "required" &&
+                          "This field is required."}
+                        {errors.name.type === "maxLength" &&
+                          "Max length is 100 char."}
+                      </p>
+                    )}
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
                   <label className="uppercase text-sm py-2">Email</label>
                   <input
                     className="border-2 rounded-lg p-3 border-gray-300"
-                    type="email"
+                    type="text"
+                    placeholder="EMAIL"
+                    {...register("email", {
+                      required: true,
+                      pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    })}
                   />
+                  {errors.email && (
+                    <p className="text-red mt-1">
+                      {errors.email.type === "required" &&
+                        "This field is required."}
+                      {errors.email.type === "pattern" &&
+                        "Invalid email address."}
+                    </p>
+                  )}
                 </div>
                 <div className="flex flex-col py-2">
                   <label className="uppercase text-sm py-2">Subject</label>
@@ -96,7 +131,22 @@ const Contact = () => {
                   <textarea
                     className="border-2 rounded-lg p-3 border-gray-300"
                     rows="10"
-                  ></textarea>
+                    name="message"
+                    placeholder="MESSAGE"
+                    cols="50"
+                    {...register("message", {
+                      required: true,
+                      maxLength: 2000,
+                    })}
+                  />
+                  {errors.message && (
+                    <p className="text-red mt-1">
+                      {errors.message.type === "required" &&
+                        "This field is required."}
+                      {errors.message.type === "maxLength" &&
+                        "Max length is 2000 char."}
+                    </p>
+                  )}
                 </div>
                 <button className="w-full p-4 text-gray-100 mt-4">
                   Send Message
@@ -106,7 +156,7 @@ const Contact = () => {
           </div>
         </div>
         <div className="flex justify-center py-12">
-          <Link href="/#home" scroll={false}>
+          <Link href="/#about" scroll={false}>
             <div className="rounded-full shadow-lg shadow-black p-4 cursor-pointer hover:scale-110 ease-in duration-300">
               <HiOutlineChevronDoubleUp className="black" size={30} />
             </div>
