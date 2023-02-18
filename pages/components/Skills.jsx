7aa62d6d5c0eeb1bf95aa26f8364225430372845
dv <dvaug86@gmail.com>
2023-02-18 13:42:12 -0500

    first fix of blinds

--- pages/components/Skills.jsx
+++ pages/components/Skills.jsx
@@ -46,7 +46,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl shadow-gray-400 bg-black border-4  border-blue-500 rounded-xl hover:scale-105 ease-in duration-300">
               <div className="flex flex-col  gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={HTML} width="64px" height="64px" alt="/"  />
+                  <Image src={HTML} width="64px" height="64px" alt="/" onLoad={() => setIsLoaded(true)} />
                 </div>
                 <div className="flex flex-col items-center justify-center">
                   <h3 className="text-3xl text-white">HTML</h3>
@@ -57,7 +57,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl shadow-gray-400 bg-black border-4  border-blue-500 rounded-xl hover:scale-105 ease-in duration-300">
               <div className="flex flex-col  gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={JavaScript} width="64px" height="64px" alt="/"  />
+                  <Image src={JavaScript} width="64px" height="64px" alt="/" onLoad={() => setIsLoaded(true)} />
                 </div>
                 <div className="flex flex-col  gap-4 justify-center items-center">
                   <h3 className="text-3xl text-white">JavaScript</h3>
@@ -79,7 +79,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl shadow-gray-400 bg-black border-4  border-blue-500 rounded-xl hover:scale-105 ease-in duration-300">
               <div className="flex flex-col  gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={NextJS} width="64px" height="64px" alt="/" />
+                  <Image src={NextJS} width="64px" height="64px" alt="/" onLoad={() => setIsLoaded(true)}/>
                 </div>
                 <div className="flex flex-col  gap-4 justify-center items-center">
                   <h3 className="text-3xl text-white">NextJS</h3>
@@ -90,7 +90,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl shadow-gray-400 bg-black border-4  border-blue-500 rounded-xl hover:scale-105 ease-in duration-300">
               <div className="flex flex-col  gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={nodeLogo} width="64px" height="64px" alt="/" />
+                  <Image src={nodeLogo} width="64px" height="64px" alt="/" onLoad={() => setIsLoaded(true)} />
                 </div>
                 <div className="flex flex-col  gap-4 justify-center items-center">
                   <h3 className="text-3xl text-white">NodeJS</h3>
@@ -101,7 +101,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl shadow-gray-400 bg-black border-4  border-blue-500 rounded-xl hover:scale-105 ease-in duration-300">
               <div className="flex flex-col  gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={CSS} width="64px" height="64px" alt="/" />
+                  <Image src={CSS} width="64px" height="64px" alt="/" onLoad={() => setIsLoaded(true)} />
                 </div>
                 <div className="flex flex-col  gap-4 justify-center items-center">
                   <h3 className="text-3xl text-white">CSS</h3>
@@ -112,7 +112,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl shadow-gray-400 bg-black border-4  border-blue-500 rounded-xl hover:scale-105 ease-in duration-300">
               <div className="flex flex-col  gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={bootstrap} width="64px" height="64px" alt="/" />
+                  <Image src={bootstrap} width="64px" height="64px" alt="/" onLoad={() => setIsLoaded(true)} />
                 </div>
                 <div className="flex flex-col  gap-4 justify-center items-center">
                   <h3 className="text-3xl text-white">Bootstrap</h3>
@@ -123,7 +123,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl shadow-gray-400 bg-black border-4  border-blue-500 rounded-xl hover:scale-105 ease-in duration-300">
               <div className="flex flex-col  gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={TailWind} width="64px" height="64px" alt="/" />
+                  <Image src={TailWind} width="64px" height="64px" alt="/" onLoad={() => setIsLoaded(true)} />
                 </div>
                 <div className="flex flex-col  gap-4 justify-center items-center">
                   <h3 className="text-3xl text-white">TailWind</h3>
@@ -134,7 +134,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl shadow-gray-400 bg-black border-4  border-blue-500 rounded-xl hover:scale-105 ease-in duration-300">
               <div className="flex flex-col  gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={mysql} width="64px" height="64px" alt="/" />
+                  <Image src={mysql} width="64px" height="64px" alt="/" onLoad={() => setIsLoaded(true)} />
                 </div>
                 <div className="flex flex-col  gap-4 justify-center items-center">
                   <h3 className="text-3xl text-white">MySQL</h3>
@@ -145,7 +145,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl shadow-gray-400 bg-black border-4  border-blue-500 rounded-xl hover:scale-105 ease-in duration-300">
               <div className="flex flex-col  gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={mongoLogo} width="64px" height="64px" alt="/" />
+                  <Image src={mongoLogo} width="64px" height="64px" alt="/" onLoad={() => setIsLoaded(true)} />
                 </div>
                 <div className="flex flex-col  gap-4 justify-center items-center">
                   <h3 className="text-3xl text-white">MongoDB</h3>
@@ -162,6 +162,7 @@ const Skills = () => {
                     width="64px"
                     height="64px"
                     alt="/"
+                    onLoad={() => setIsLoaded(true)}
                   />
                 </div>
                 <div className="flex flex-col  gap-4 justify-center items-center">
